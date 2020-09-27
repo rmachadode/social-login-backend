621bbb3a2f504d1c1b244d9534e698fe6f9639d4
Rodrigo Machado <rmachado@overactiveinc.com>
2020-09-27 08:41:29 -0300

    Creación del proyecto

---
+++ src/main/java/com/terodata/social/login/app/SocialLoginBackendApplication.java
@@ -0,0 +1,13 @@
+package com.terodata.social.login.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terodata/social/login/app/SocialLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terodata.social.login.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
